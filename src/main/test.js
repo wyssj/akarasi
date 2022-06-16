3acf92aca5ec6097217d4e94fd787bb91ad34590
wyssj <jannik.wyss@stud.unibas.ch>
2022-06-17 01:43:47 +0200

    playing with canvas and webgl

---
+++ src/main/test.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    var c = document.getElementById("canvas1");
+    let ctx = c.getContext("2d");
+
+    ctx.strokeStyle = 'white';
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+
+    var c = document.getElementById("canvas2");
+    ctx = c.getContext("2d");
+    ctx.strokeStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+
+    var c = document.getElementById("canvas3");
+    ctx = c.getContext("2d");
+    ctx.fillStyle = "white";
+    ctx.font = "30px Arial";
+    ctx.fillText("Hello World", 10, 50);
+}
